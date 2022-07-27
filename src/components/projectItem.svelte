13fcd72f6fc398f40c2b11a45a3cf806151caca3
Jade Nash <jade.nashh18@gmail.com>
2022-07-27 16:57:45 -0500

    +overflow-visible on glow div

--- src/components/projectItem.svelte
+++ src/components/projectItem.svelte
@@ -14,7 +14,7 @@
 <div class="relative group overflow-visible">
 	<!-- Card Background Glow -->
 	<div
-		class="absolute inset-0 card w-xl bg-gradient-to-br from-pink-600 to-purple-300 blur-md opacity-80 group-hover:opacity-100 transition duration-200"
+		class="overflow-visible absolute inset-0 card w-xl bg-gradient-to-br from-pink-600 to-purple-300 blur-md opacity-80 group-hover:opacity-100 transition duration-200"
 	/>
 	<!-- Card Base -->
 	<div class="card w-xl bg-base-100 shadow-xl overflow-visible">
