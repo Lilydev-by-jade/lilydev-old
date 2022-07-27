8a5a81a0c00544ab3dee5b4dfebf2d264bff79b5
Jade Nash <jade.nashh18@gmail.com>
2022-07-27 07:03:28 -0500

    basic project page setup/styling

---
+++ src/components/projectItem.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Modrinth from '../components/icons/modrinth.svg?url';
+
+	export let title: String = 'Card title!';
+	export let description: String = 'If a dog chews shoes whose shoes does he choose?';
+	export let githubUrl: any = null;
+	export let modrinthUrl: any = null;
+</script>
+
+<div class="relative group">
+	<!-- Card Background Glow -->
+	<div
+		class="absolute inset-0 card w-96 bg-gradient-to-br from-pink-600 to-pink-400 blur-md opacity-80 group-hover:opacity-100 transition duration-200"
+	/>
+	<!-- Card Base -->
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<div class="card-body pb-5">
+			<h2 class="card-title">{title}</h2>
+
+			<!-- TODO: add project type across from card title. (maybe specify mod-loader?) -->
+			<p>{description}</p>
+			<div class="card-actions justify-end items-center">
+				{#if githubUrl != null}
+					<a href={githubUrl}>
+						<div class="tooltip" data-tip="GitHub">
+							<i class="fa-brands fa-github text-white text-3xl" />
+						</div>
+					</a>
+				{/if}
+				{#if modrinthUrl != null}
+					<a href={modrinthUrl}>
+						<div class="tooltip flex items-center" data-tip="Modrinth">
+							<img src={Modrinth} width="30" alt="modrinth logo icon" />
+						</div>
+					</a>
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/routes/projects.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ProjectItem from '../components/projectItem.svelte';
+	import Modrinth from '../components/icons/modrinth.svg?url';
+</script>
+
+<div class="grid grid-cols-12 grid-flow-row justify-items-center space-y-12">
+	<h1 class="col-span-12 text-4xl w-auto">Projects</h1>
+	<div
+		class="flex justify-center col-span-12 space-y-6 xl:w-4/12 lg:w-7/12 md:w-9/12 sm:w-10/12 w-10/12"
+	>
+		<ProjectItem
+			title="VoluBind"
+			description="A client-side Minecraft mod that allows you to create keybinds to control your in-game volume."
+			githubUrl="https://github.com/jadelily18/VoluBind"
+		/>
+	</div>
+</div>
