08a6bd3975e880c9e66283b75ba61cbc2720249b
Jade Nash <jade.nashh18@gmail.com>
2022-08-01 09:47:41 -0500

    code refactor + added personal branding

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -1,6 +1,13 @@
+<script lang="ts">
+	import LilydevLogo from '$lib/branding/lilydevLogo.svelte';
+</script>
+
 <div class="navbar bg-base-100 px-8">
 	<div class="flex-1">
-		<a class="btn btn-ghost normal-case text-xl" href="/">Lilydev_</a>
+		<a class="btn btn-ghost normal-case space-x-1" href="/">
+			<LilydevLogo svgWidth="34" svgFill="#B3CCF5" />
+			<h1 class="text-2xl font-light">LILYDEV</h1>
+		</a>
 	</div>
 	<div class="flex-none">
 		<ul class="menu menu-horizontal p-0">
